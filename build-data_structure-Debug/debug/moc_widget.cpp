af37c5b55b75233a60dae831c2a4a1400a7aaf15
czy20000902 <60259729+czy20000902@users.noreply.github.com>
2023-01-02 00:28:37 +0100

    Add files via upload

---
+++ build-data_structure-Debug/debug/moc_widget.cpp
@@ -0,0 +1,339 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'widget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../data_structure/widget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'widget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Widget_t {
+    QByteArrayData data[48];
+    char stringdata0[1145];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Widget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Widget_t qt_meta_stringdata_Widget = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Widget"
+QT_MOC_LITERAL(1, 7, 23), // "on_pushButton_0_clicked"
+QT_MOC_LITERAL(2, 31, 0), // ""
+QT_MOC_LITERAL(3, 32, 23), // "on_pushButton_1_clicked"
+QT_MOC_LITERAL(4, 56, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(5, 80, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(6, 104, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(7, 128, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(8, 152, 23), // "on_pushButton_6_clicked"
+QT_MOC_LITERAL(9, 176, 23), // "on_pushButton_7_clicked"
+QT_MOC_LITERAL(10, 200, 23), // "on_pushButton_8_clicked"
+QT_MOC_LITERAL(11, 224, 23), // "on_pushButton_9_clicked"
+QT_MOC_LITERAL(12, 248, 24), // "on_pushButton_10_clicked"
+QT_MOC_LITERAL(13, 273, 24), // "on_pushButton_11_clicked"
+QT_MOC_LITERAL(14, 298, 24), // "on_pushButton_12_clicked"
+QT_MOC_LITERAL(15, 323, 24), // "on_pushButton_13_clicked"
+QT_MOC_LITERAL(16, 348, 24), // "on_pushButton_14_clicked"
+QT_MOC_LITERAL(17, 373, 24), // "on_pushButton_15_clicked"
+QT_MOC_LITERAL(18, 398, 24), // "on_pushButton_16_clicked"
+QT_MOC_LITERAL(19, 423, 24), // "on_pushButton_17_clicked"
+QT_MOC_LITERAL(20, 448, 24), // "on_pushButton_18_clicked"
+QT_MOC_LITERAL(21, 473, 24), // "on_pushButton_19_clicked"
+QT_MOC_LITERAL(22, 498, 24), // "on_pushButton_20_clicked"
+QT_MOC_LITERAL(23, 523, 24), // "on_pushButton_21_clicked"
+QT_MOC_LITERAL(24, 548, 24), // "on_pushButton_22_clicked"
+QT_MOC_LITERAL(25, 573, 24), // "on_pushButton_23_clicked"
+QT_MOC_LITERAL(26, 598, 24), // "on_pushButton_24_clicked"
+QT_MOC_LITERAL(27, 623, 24), // "on_pushButton_25_clicked"
+QT_MOC_LITERAL(28, 648, 24), // "on_pushButton_26_clicked"
+QT_MOC_LITERAL(29, 673, 24), // "on_pushButton_27_clicked"
+QT_MOC_LITERAL(30, 698, 24), // "on_pushButton_28_clicked"
+QT_MOC_LITERAL(31, 723, 24), // "on_pushButton_29_clicked"
+QT_MOC_LITERAL(32, 748, 24), // "on_pushButton_30_clicked"
+QT_MOC_LITERAL(33, 773, 24), // "on_pushButton_31_clicked"
+QT_MOC_LITERAL(34, 798, 24), // "on_pushButton_32_clicked"
+QT_MOC_LITERAL(35, 823, 24), // "on_pushButton_33_clicked"
+QT_MOC_LITERAL(36, 848, 24), // "on_pushButton_37_clicked"
+QT_MOC_LITERAL(37, 873, 24), // "on_pushButton_38_clicked"
+QT_MOC_LITERAL(38, 898, 24), // "on_pushButton_39_clicked"
+QT_MOC_LITERAL(39, 923, 24), // "on_pushButton_40_clicked"
+QT_MOC_LITERAL(40, 948, 24), // "on_pushButton_41_clicked"
+QT_MOC_LITERAL(41, 973, 24), // "on_pushButton_42_clicked"
+QT_MOC_LITERAL(42, 998, 24), // "on_pushButton_43_clicked"
+QT_MOC_LITERAL(43, 1023, 24), // "on_pushButton_44_clicked"
+QT_MOC_LITERAL(44, 1048, 24), // "on_pushButton_45_clicked"
+QT_MOC_LITERAL(45, 1073, 24), // "on_pushButton_46_clicked"
+QT_MOC_LITERAL(46, 1098, 24), // "on_pushButton_47_clicked"
+QT_MOC_LITERAL(47, 1123, 21) // "on_pushButton_clicked"
+
+    },
+    "Widget\0on_pushButton_0_clicked\0\0"
+    "on_pushButton_1_clicked\0on_pushButton_2_clicked\0"
+    "on_pushButton_3_clicked\0on_pushButton_4_clicked\0"
+    "on_pushButton_5_clicked\0on_pushButton_6_clicked\0"
+    "on_pushButton_7_clicked\0on_pushButton_8_clicked\0"
+    "on_pushButton_9_clicked\0"
+    "on_pushButton_10_clicked\0"
+    "on_pushButton_11_clicked\0"
+    "on_pushButton_12_clicked\0"
+    "on_pushButton_13_clicked\0"
+    "on_pushButton_14_clicked\0"
+    "on_pushButton_15_clicked\0"
+    "on_pushButton_16_clicked\0"
+    "on_pushButton_17_clicked\0"
+    "on_pushButton_18_clicked\0"
+    "on_pushButton_19_clicked\0"
+    "on_pushButton_20_clicked\0"
+    "on_pushButton_21_clicked\0"
+    "on_pushButton_22_clicked\0"
+    "on_pushButton_23_clicked\0"
+    "on_pushButton_24_clicked\0"
+    "on_pushButton_25_clicked\0"
+    "on_pushButton_26_clicked\0"
+    "on_pushButton_27_clicked\0"
+    "on_pushButton_28_clicked\0"
+    "on_pushButton_29_clicked\0"
+    "on_pushButton_30_clicked\0"
+    "on_pushButton_31_clicked\0"
+    "on_pushButton_32_clicked\0"
+    "on_pushButton_33_clicked\0"
+    "on_pushButton_37_clicked\0"
+    "on_pushButton_38_clicked\0"
+    "on_pushButton_39_clicked\0"
+    "on_pushButton_40_clicked\0"
+    "on_pushButton_41_clicked\0"
+    "on_pushButton_42_clicked\0"
+    "on_pushButton_43_clicked\0"
+    "on_pushButton_44_clicked\0"
+    "on_pushButton_45_clicked\0"
+    "on_pushButton_46_clicked\0"
+    "on_pushButton_47_clicked\0on_pushButton_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Widget[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      46,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  244,    2, 0x08 /* Private */,
+       3,    0,  245,    2, 0x08 /* Private */,
+       4,    0,  246,    2, 0x08 /* Private */,
+       5,    0,  247,    2, 0x08 /* Private */,
+       6,    0,  248,    2, 0x08 /* Private */,
+       7,    0,  249,    2, 0x08 /* Private */,
+       8,    0,  250,    2, 0x08 /* Private */,
+       9,    0,  251,    2, 0x08 /* Private */,
+      10,    0,  252,    2, 0x08 /* Private */,
+      11,    0,  253,    2, 0x08 /* Private */,
+      12,    0,  254,    2, 0x08 /* Private */,
+      13,    0,  255,    2, 0x08 /* Private */,
+      14,    0,  256,    2, 0x08 /* Private */,
+      15,    0,  257,    2, 0x08 /* Private */,
+      16,    0,  258,    2, 0x08 /* Private */,
+      17,    0,  259,    2, 0x08 /* Private */,
+      18,    0,  260,    2, 0x08 /* Private */,
+      19,    0,  261,    2, 0x08 /* Private */,
+      20,    0,  262,    2, 0x08 /* Private */,
+      21,    0,  263,    2, 0x08 /* Private */,
+      22,    0,  264,    2, 0x08 /* Private */,
+      23,    0,  265,    2, 0x08 /* Private */,
+      24,    0,  266,    2, 0x08 /* Private */,
+      25,    0,  267,    2, 0x08 /* Private */,
+      26,    0,  268,    2, 0x08 /* Private */,
+      27,    0,  269,    2, 0x08 /* Private */,
+      28,    0,  270,    2, 0x08 /* Private */,
+      29,    0,  271,    2, 0x08 /* Private */,
+      30,    0,  272,    2, 0x08 /* Private */,
+      31,    0,  273,    2, 0x08 /* Private */,
+      32,    0,  274,    2, 0x08 /* Private */,
+      33,    0,  275,    2, 0x08 /* Private */,
+      34,    0,  276,    2, 0x08 /* Private */,
+      35,    0,  277,    2, 0x08 /* Private */,
+      36,    0,  278,    2, 0x08 /* Private */,
+      37,    0,  279,    2, 0x08 /* Private */,
+      38,    0,  280,    2, 0x08 /* Private */,
+      39,    0,  281,    2, 0x08 /* Private */,
+      40,    0,  282,    2, 0x08 /* Private */,
+      41,    0,  283,    2, 0x08 /* Private */,
+      42,    0,  284,    2, 0x08 /* Private */,
+      43,    0,  285,    2, 0x08 /* Private */,
+      44,    0,  286,    2, 0x08 /* Private */,
+      45,    0,  287,    2, 0x08 /* Private */,
+      46,    0,  288,    2, 0x08 /* Private */,
+      47,    0,  289,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Widget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_0_clicked(); break;
+        case 1: _t->on_pushButton_1_clicked(); break;
+        case 2: _t->on_pushButton_2_clicked(); break;
+        case 3: _t->on_pushButton_3_clicked(); break;
+        case 4: _t->on_pushButton_4_clicked(); break;
+        case 5: _t->on_pushButton_5_clicked(); break;
+        case 6: _t->on_pushButton_6_clicked(); break;
+        case 7: _t->on_pushButton_7_clicked(); break;
+        case 8: _t->on_pushButton_8_clicked(); break;
+        case 9: _t->on_pushButton_9_clicked(); break;
+        case 10: _t->on_pushButton_10_clicked(); break;
+        case 11: _t->on_pushButton_11_clicked(); break;
+        case 12: _t->on_pushButton_12_clicked(); break;
+        case 13: _t->on_pushButton_13_clicked(); break;
+        case 14: _t->on_pushButton_14_clicked(); break;
+        case 15: _t->on_pushButton_15_clicked(); break;
+        case 16: _t->on_pushButton_16_clicked(); break;
+        case 17: _t->on_pushButton_17_clicked(); break;
+        case 18: _t->on_pushButton_18_clicked(); break;
+        case 19: _t->on_pushButton_19_clicked(); break;
+        case 20: _t->on_pushButton_20_clicked(); break;
+        case 21: _t->on_pushButton_21_clicked(); break;
+        case 22: _t->on_pushButton_22_clicked(); break;
+        case 23: _t->on_pushButton_23_clicked(); break;
+        case 24: _t->on_pushButton_24_clicked(); break;
+        case 25: _t->on_pushButton_25_clicked(); break;
+        case 26: _t->on_pushButton_26_clicked(); break;
+        case 27: _t->on_pushButton_27_clicked(); break;
+        case 28: _t->on_pushButton_28_clicked(); break;
+        case 29: _t->on_pushButton_29_clicked(); break;
+        case 30: _t->on_pushButton_30_clicked(); break;
+        case 31: _t->on_pushButton_31_clicked(); break;
+        case 32: _t->on_pushButton_32_clicked(); break;
+        case 33: _t->on_pushButton_33_clicked(); break;
+        case 34: _t->on_pushButton_37_clicked(); break;
+        case 35: _t->on_pushButton_38_clicked(); break;
+        case 36: _t->on_pushButton_39_clicked(); break;
+        case 37: _t->on_pushButton_40_clicked(); break;
+        case 38: _t->on_pushButton_41_clicked(); break;
+        case 39: _t->on_pushButton_42_clicked(); break;
+        case 40: _t->on_pushButton_43_clicked(); break;
+        case 41: _t->on_pushButton_44_clicked(); break;
+        case 42: _t->on_pushButton_45_clicked(); break;
+        case 43: _t->on_pushButton_46_clicked(); break;
+        case 44: _t->on_pushButton_47_clicked(); break;
+        case 45: _t->on_pushButton_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Widget::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_Widget.data,
+    qt_meta_data_Widget,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Widget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Widget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Widget.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 46)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 46;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 46)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 46;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
